1bc6294e9c653fddc6b3abc68c36b6cdd3983459
[JxaxJ] <[klekklaks@gmail.com]>
2022-09-19 15:27:34 +0300

    V0.2 Hotfix 17.09.22

--- MODULE/install_module.py
+++ MODULE/install_module.py
@@ -2,12 +2,12 @@ import os
 
 
 def install_module():
-    libs = {"numpy", "tensorflow-gpu", "tensorflow", 'keras', 'pandas'}
+    libs = {'--upgrade pip', "numpy", "tensorflow-gpu", "tensorflow", 'keras', 'pandas'}
 
     try:
         for lib in libs:
             print("start install {0}".format(lib))
-            os.system("pip install " + lib)
+            os.system(f"pip install {lib}")
             print("{} install successful".format(lib))
         print("All Successful")
 
